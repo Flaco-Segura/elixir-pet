f0fcb354c9322efcf96d9600c431f6468cc7d5a5
Flaco-Segura <manuel.quesada@sngular.com>
2024-08-07 07:53:53 +0200

    [EPT-07080753] patterns

---
+++ examples/lib/examples/patterns/boolean_case.ex
@@ -0,0 +1,11 @@
+defmodule Examples.Patterns.BooleanCase do
+  def to_bool(value) do
+    case value do
+      bool when is_boolean(bool) -> bool
+      0 -> false
+      nil -> false
+      str when str in ["undefined", "false"] -> false 
+      _ -> true
+    end
+  end
+end
